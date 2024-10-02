9faa3af0f95b97e18168a7637fbdb1ae810e75f8
AndiFachryAlamTengko <fachry.classroom@gmail.com>
2024-10-02 12:10:45 +0800

    Add files via upload

---
+++ Praktikum-APD/Post-Test/Pertemuan_5.py
@@ -0,0 +1,123 @@
+#0 itu indeks kalau start itu 0 stop berenti tpi dimulai dri 1
+
+nama = ["celio","shandy","farel","ghazali","vito","rizal","adri","adi","ifnu"]
+matkul = ["APD","APL","BASDAT","STRUKDAT","WEB","JARKOM"]
+data = nama+matkul
+
+#buat print nama
+# print (nama)
+
+#buat print nama dan tambah nama di akhir (membuat)
+print("sebelum: ")
+print (nama)
+print("")
+print ("sesudah: ")
+nama.append("afrizal")
+print(nama)
+print(data*3)
+
+#buat tambah nama di tengah (membuat)
+print("sebelum: ")
+print (nama)
+print("")
+print ("sesudah: ")
+nama.append(2, "afrizal")
+print(nama)
+
+#cara menimpa atau mengubah sebuah elemen (update)
+nama[4]= "nanaa"
+print(nama)
+
+#menghapus sebuah elemen (fungsi del) 
+del nama[2]
+print(nama)
+##memindahkan sebuah elemen (fungsi pop )
+hapus = nama.pop(2)
+print(nama)
+
+#memanggil indeks dan start
+print(nama[0:2])
+print(nama[1.9])
+
+#memanggil indeks dan start tapi melompati(step)
+print(nama[1:9:2])
+
+
+data = ["farel","celio",[1,2,["halo"]]]
+print(data[2][1][0])
+data = ["farel","celio",[1,2,["halo",23,2,3.True]]]
+print(data[2][2][0])
+#negatif
+data = ["farel","celio",[1,2,["halo",23,2,3.True]]]
+print(data[2][2][::-1])
+
+nama = ["celio",
+
+"shandy",
+
+"farel",
+
+"ghazali",
+
+"vito",
+
+"adri",
+
+"ifnu"
+
+"tes",
+
+"isal"
+
+]
+
+matkul = ["APD", "APL", "BASDAT", "STRUKDAT", "WEB", "JARKOM"]
+
+data = nama+matkul
+
+print("sebelum: ")
+
+print (nama[2:5])
+
+print ("")
+
+print ("sesudah")
+
+print(data*3)
+
+# nama.insert(2, "afrizal")
+
+# nama.append("afrizal")
+
+# print(nama)
+
+# nama[4]="fufufafa"
+
+# del nama [2]
+
+# hapus nama.pop(2)
+
+# print (nama)
+
+# print (hapus)
+
+# print(nama [0:2])
+
+# print (nama [1:9:2])
+
+data = ["farel","celio",[]]
+
+matkul = [1,2,3,4,[5, 6,333]]
+
+
+print(
+      =========================
+    |                       |
+    |                       |
+    |                       |
+    |                       |
+    |                       |
+    |                       |
+    |                       |
+    |                       |
+)  
