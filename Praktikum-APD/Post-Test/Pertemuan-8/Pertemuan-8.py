475f482568f32edd572752081d2cd3c008a40946
AndiFachryAlamTengko <fachry.classroom@gmail.com>
2024-10-23 08:41:33 +0800

    Add files via upload

---
+++ Praktikum-APD/Post-Test/Pertemuan-8/Pertemuan-8.py
@@ -0,0 +1,98 @@
+# if
+
+# list - [1,2,3]
+
+# print(list[0])
+
+# x = 0
+# assert x != 0
+
+# x - 12
+# x.append(10)
+
+# import os
+
+# os.system("PAUSE")
+
+# dict = {
+# 'key' : 1
+# }
+
+#print(dict['lele'])
+
+#x ?
+
+# print (x)
+
+# num = 10
+#  print('hello' + num)
+
+# if num == 10:
+#     print(num)
+
+# x = 1
+# y = 0
+# print (x/y) #error
+
+# try:
+#     angka = int(input("Masukkan angka : "))
+# except ValueError:
+#     print ("Input salah") #jika error
+# else:
+#     print("selesai") #selebihnya
+# finally:
+#     print("Finale")
+
+# try:
+#     nama = str(input("Nama anda : .."))
+#     if len(nama) > 5:
+#         raise ValueError ("nama tidak boleh lebih dari 5 char")
+    
+# except VallueError as e:
+#     printt(e)
+
+# try:
+#     nim = input("Masukkan NIM anda")
+
+#     if not nim:
+#         raise ValueError("NIM tidak boleh kosong")
+#     if not nim.isdigit():
+#         raise ValueError("NIM tidak boleh kosong")
+#     if len(nim) != 10:
+#         raise ValueError ("NIM harus 10 digit")
+    
+# except TypeError as T:
+#     print(T)
+# except ValueError as E:
+#     print(E)
+
+# else:
+#     print(f'NIM Anda adalah {nim}')
+# finally:
+#     print("Program selesai...")
+
+# path ='./folder_ini/data.txt'
+# file = open(path, 'w')
+
+# with open(path, 'r') as file:
+    # konten = file.read()
+    # print(konten)
+    # for i  in file:
+    #     print(i, end='')
+
+# path = './folder_ini/data.json'
+
+# with open(path, 'w') as file:
+#     file.write('shandy,20,pria\n')
+#     file.write('shandy,19,wanita')
+
+# with open(path, 'r') as file:
+#     konten = file.read()
+#     print(konten)e
+
+
+
+
+
+
+
